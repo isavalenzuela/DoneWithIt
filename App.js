1aabddd6b4c9cd075547ed28116179a6fb55984a
isavalenzuela <isadoravalenzuelafierro@gmail.com>
2022-11-29 16:04:22 -0300

    initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  let x = 1;
+  console.log('app executed');
+
+  return (
+    <View style={styles.container}>
+      <Text>ola ke ase?</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
