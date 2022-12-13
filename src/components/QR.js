cb2aa7a9c3514c7226445abdfea20fc4ad817e20
isavalenzuela <isadoravalenzuelafierro@gmail.com>
2022-12-13 02:27:18 -0300

    add mock data for payments tab
    wip

---
+++ src/components/QR.js
@@ -0,0 +1,11 @@
+// import React from 'react';
+// import QRCode from 'react-native-qrcode-svg';
+
+// // Simple usage, defaults for all but the value
+// render() {
+//   return (
+//     <QRCode
+//       value="http://awesome.link.qr"
+//     />
+//   );
+// };
--- src/pages/PaymentScreen.js
+++ src/pages/PaymentScreen.js
@@ -1,12 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View, Button, Image } from 'react-native'
-import { Provider as PaperProvider } from 'react-native-paper'
+import { StyleSheet, View, Button, Image, Icon, Linking } from 'react-native'
+import { Text, Card } from '@rneui/themed'
 
 export function PaymentScreen({ navigation }) {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Payment</Text>
+      <Card>
+        <Card.Image
+          style={{ padding: 0, height: 280, maxWidth: '100%' }}
+          source={require('../../assets/Rickrolling_QR_code.png')}
+        />
+        <Button
+          icon={
+            <Icon name="code" color="#ffffff" iconStyle={{ marginRight: 10 }} />
+          }
+          buttonStyle={{
+            borderRadius: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginBottom: 0,
+          }}
+          title="Abrir Compraquí BancoEstado"
+          onPress={() => {
+            Linking.openURL('whatsapp://app')
+          }}
+        />
+      </Card>
     </View>
   )
 }
